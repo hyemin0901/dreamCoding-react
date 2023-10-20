dbedf5c2c8d6c959b0156cbaf5012f9803dfa331
hyemin0901 <adonis2ha@naver.com>
2023-10-20 11:56:47 +0900

    feat: test

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export default function Profile() {
   return (
-    <div className='profile'>
+    <div className="profile">
       <img
-        className='photo'
-        src='https://elements-cover-images-0.imgix.net/d205c50d-0c2a-47d1-b28c-b47e31fec079?auto=compress%2Cformat&fit=max&w=1019&s=b5cd3791b4d6903c8fcd9c4f42d9b6ae'
-        alt='motorcycle'
+        className="photo"
+        src="https://elements-cover-images-0.imgix.net/d205c50d-0c2a-47d1-b28c-b47e31fec079?auto=compress%2Cformat&fit=max&w=1019&s=b5cd3791b4d6903c8fcd9c4f42d9b6ae"
+        alt="motorcycle"
       />
       <h1>James Kim</h1>
       <p>프론트엔드 개발자</p>
+      <p>먹고 살기 힘들구만!</p>
     </div>
   );
 }
